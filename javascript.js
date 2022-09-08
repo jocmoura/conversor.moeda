d0edac4b96da266ade5c8d73454dbd8464e40f21
jonas moura <jonas.cmourag@gmail.com>
2022-09-08 07:30:26 -0300

    Adicionado
    Conversor de Pesos Argentinos adicionado.

--- javascript.js
+++ javascript.js
@@ -9,3 +9,18 @@ function Converter() {
     var valorConvertido = "O valor em Real é : R$" + valorEmReal
     elementoValorConvertido.innerHTML = valorConvertido
   }
+
+  
+  function ConverterPesos() {
+    var valorElemento = document.getElementById("valor2") // pega o valor pelo ID 
+
+    var valor = valorElemento.value;
+    var valorEmReal = parseFloat(valor);
+
+    var valorEmPesosArgentino = valorEmReal * 26.8 ;
+    console.log(valorEmPesosArgentino);
+    var elementoValorConvertido2 = document.getElementById("valorConvertido2")
+    valorConvertido2 = "O valor em Pesos Argentino é: " + valorEmPesosArgentino
+    elementoValorConvertido2.innerHTML = valorConvertido2
+
+  }
